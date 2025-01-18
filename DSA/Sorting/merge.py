9923a44ef840e66e87f9cb401410e82603291c5d
Stathis <96146295+sta8isznt@users.noreply.github.com>
2025-01-18 16:46:17 +0200

    Update merge.py

--- DSA/Sorting/merge.py
+++ DSA/Sorting/merge.py
@@ -1,40 +1,37 @@
 def merge_sort(arr):
-    if len(arr) > 1:
-        # Divide the array into two halves
-        mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
+    if len(arr) <= 1:
+        return arr
+    left = 0
+    right = len(arr)
+    mid = (left + right) // 2 # mid = len(arr) // 2
 
-        # Recursively sort both halves
-        merge_sort(left_half)
-        merge_sort(right_half)
+    left_half = arr[:mid]
+    right_half = arr[mid:]
 
-        # Merge the sorted halves
-        i = j = k = 0
+    merge_sort(left_half)
+    merge_sort(right_half)
 
-        # Copy data to the original array while sorting
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
-
-        # Copy any remaining elements of left_half
-        while i < len(left_half):
+    i = j = k = 0
+    while i < len(left_half) and j < len(right_half):
+        if left_half[i] < right_half[j]:
             arr[k] = left_half[i]
             i += 1
-            k += 1
-
-        # Copy any remaining elements of right_half
-        while j < len(right_half):
+        else:
             arr[k] = right_half[j]
-            j += 1
-            k += 1
+            j+=1
+        k += 1
 
-        print(f"Step: {arr}")
+    while i < len(left_half):
+        arr[k] = left_half[i]
+        i += 1
+        k += 1
+
+    while j < len(right_half):
+        arr[k] = right_half[j]
+        j += 1
+        k += 1
+
+
 
 array = [64, 34, 25, 12, 22, 11, 90]
 merge_sort(array)
