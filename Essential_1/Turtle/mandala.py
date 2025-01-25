df182b7a647021372062c825b6e7f90ba8a29635
Stathis <96146295+sta8isznt@users.noreply.github.com>
2025-01-25 12:37:10 +0200

    25/1

--- Essential_1/Turtle/mandala.py
+++ Essential_1/Turtle/mandala.py
@@ -3,9 +3,12 @@ import turtle
 turtle.setup(800, 800)
 turtle.speed(0)
 turtle.bgcolor("skyblue")
-nikos = turtle.Turtle()
-
+turtle.hideturtle()
 colors = ["green", "red", "yellow", "blue", "orange", "purple"]
-for i in range(36):
+for i in range(72):
     color = colors[i % len(colors)]
+    turtle.color(color)
+    turtle.circle(100)
+    turtle.right(5)
 
+turtle.done()
