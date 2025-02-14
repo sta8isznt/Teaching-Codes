881493a4d9145448167f1bcb2699e6644038c4cd
Stathis <96146295+sta8isznt@users.noreply.github.com>
2025-02-14 17:14:43 +0200

    14/2

--- Advanced_3/DSA/Stacks n Queues/utils.py
+++ Advanced_3/DSA/Stacks n Queues/utils.py
@@ -13,3 +13,19 @@ class Stack:
         return self.stack[-1]
     def size(self):
         return len(self.stack)
+    
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, item):
+        self.queue.append(item)
+    def is_empty(self):
+        return len(self.queue) == 0
+    def dequeue(self):
+        if not self.is_empty():
+            return self.queue.pop(0)
+        return -1
+    def peek(self):
+        return self.queue[0]
+    def size(self):
+        return len(self.queue)
